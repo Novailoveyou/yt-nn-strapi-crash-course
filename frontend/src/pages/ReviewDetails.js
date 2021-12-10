f0dfb28ef3d3654d891b10c07414c8b998ebabfa
novailoveyou <novailoveyoux@gmail.com>
2021-12-10 14:58:39 +0300

    fetching a single record

--- frontend/src/pages/ReviewDetails.js
+++ frontend/src/pages/ReviewDetails.js
@@ -1,5 +1,29 @@
+import { useParams, useNavigate, Navigate } from 'react-router'
+import useFetch from '../hooks/useFetch'
+
 const ReviewDetails = () => {
-  return <div>Review Details</div>
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const { loading, error, data } = useFetch(
+    `http://localhost:1337/api/reviews/${id}`
+  )
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  console.log(data)
+
+  return (
+    <div className='review-card'>
+      <div className='rating'>{data.attributes.rating}</div>
+      <h2>{data.attributes.title}</h2>
+      <small>console list</small>
+      <p>{data.attributes.body}</p>
+      <button className='btn' onClick={() => navigate(-1)}>
+        Go back
+      </button>
+    </div>
+  )
 }
 
 export default ReviewDetails
