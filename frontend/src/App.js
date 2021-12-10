6c766b9bd8424ca09f257b1516c9ab6414a6be9a
novailoveyou <novailoveyoux@gmail.com>
2021-12-10 13:51:09 +0300

    added frontend & split backend and frontend to separate directories & created basic navigation & some components to start with

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Homepage from './pages/Homepage'
+import ReviewDetails from './pages/ReviewDetails'
+import Category from './pages/Category'
+import SiteHeader from './components/SiteHeader'
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <SiteHeader />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/details/:id' element={<ReviewDetails />} />
+          <Route path='/category/:id' element={<Category />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
